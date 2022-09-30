b75f342708b7a5486b25ac9e9da19e0217e93631
Russell Elliott <russell@quadsquad.net>
2022-09-29 17:32:26 -0700

    able to read; getting ready to write

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
+
+import pandas as pd
+
+import os
+from dotenv import load_dotenv
+load_dotenv() #take environment variables from .env
+
+#get the google sheet id
+id = os.environ.get("ID")#google sheet ID
+
+def getFollowers(username):
+    pass
+
 if __name__ == "__main__":
-   print("Hello")
+   #initialize the dataframe
+    df = pd.read_csv(f"https://docs.google.com/spreadsheets/d/{id}/export?format=csv")
+
+    print(df) #print the dataframe
