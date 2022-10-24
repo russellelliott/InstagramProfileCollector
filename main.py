9a5e9dc68cb59a75904d8228cf99fe800725f460
Russell Elliott <russell@quadsquad.net>
2022-10-24 12:57:37 -0700

    able to put ids in file; will modify to do usernames instead

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def getConnections(username):
     #write all the ids to a text file for future use
     #join together
     connectionID = followers + following
-    fileName = username + ".txt"
+    fileName = username + "-user-ids" + ".txt" #ex: ucsc-user-ids.txt
     with open(fileName, "w") as f:
         for i in connectionID:
             f.write(i + "\n") #to write line, manually need to add newline character
@@ -42,14 +42,25 @@ def getConnections(username):
 if __name__ == "__main__":
     if(os.path.exists("config")):
         shutil.rmtree("config") #delete the config folder; remedy for some errors
-    bot = Bot()
-    bot.login(username=USERNAME, password=PASS) #need to login
-    #initialize the dataframe
-    #df = pd.read_csv(f"https://docs.google.com/spreadsheets/d/{id}/export?format=csv")
+    
+    print("INSTAGRAM PROFILE COLLECTOR")
+    print("Type 'collect' to collect the IDs of people following the UCSC Instagram page")
+    print("Type 'parse' to parse through the follower IDs")
+    action = input("Select action: ")
+    
+    if(action == "collect"):
+        bot = Bot()
+        bot.login(username=USERNAME, password=PASS) #need to login
+        #initialize the dataframe
+        #df = pd.read_csv(f"https://docs.google.com/spreadsheets/d/{id}/export?format=csv")
 
-    #print(df) #print the dataframe
-    #getConnections("ucsc")
-    user_id = bot.get_user_id_from_username("ucsc")
-    user_info = bot.get_user_info(user_id)
-    print(user_info['biography'])
-    getConnections("ucsc")
+        #print(df) #print the dataframe
+        #getConnections("ucsc")
+        user_id = bot.get_user_id_from_username("ucsc")
+        user_info = bot.get_user_info(user_id)
+        print(user_info['biography'])
+        getConnections("ucsc")
+    elif(action == "parse"):
+        print("will parse")
+    else:
+        print("Invalid action. Exiting program")
